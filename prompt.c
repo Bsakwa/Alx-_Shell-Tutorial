227006904311752a804ebf11b14227e830824b44
Bsakwa <sakwabrian23@gmail.com>
2022-11-07 13:01:47 +0000

    getline

---
+++ prompt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Prints"$" and waits for user to enter command
+  *
+  * return: 0
+  */
+int main(void)
+{
+	char *buffer = NULL;
+	size_t len = 0;
+
+	if (getline(&buffer, &len, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			printf("$ ");
+			getline(&buffer, &len, stdin);
+			printf("%s", buffer);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			exit(EXIT_FAILURE);
+			return (-1);
+		}
+	}
+	return (*buffer);
+}
