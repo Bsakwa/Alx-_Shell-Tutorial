7e892b64195b195d14d35bb2637db7302eff3350
Bsakwa <sakwabrian23@gmail.com>
2022-11-07 11:43:28 +0000

    ppid

---
+++ getppid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * main - Returns the PID of the parent process
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	pid_t my_pid;
+	my_pid = getppid();
+	printf("%u\n",my_pid);
+
+	return (0);
+}
+
+
